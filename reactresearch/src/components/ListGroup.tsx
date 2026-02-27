5fa64c7e25b0f73e97b85b37d62a6d9e91634a3e
Bruce <czp18@txstate.edu>
2026-02-26 18:17:50 -0600

    Added dividers to react items

--- reactresearch/src/components/ListGroup.tsx
+++ reactresearch/src/components/ListGroup.tsx
@@ -2,7 +2,6 @@ import { Fragment } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
-  items = [];
 
   return (
     <>
@@ -10,7 +9,9 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
