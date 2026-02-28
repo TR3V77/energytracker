e28d3a793c9b1784c548974119e371d2ed5cd779
Bruce <czp18@txstate.edu>
2026-02-27 20:22:16 -0600

    Added Handle click

--- reactresearch/src/components/ListGroup.tsx
+++ reactresearch/src/components/ListGroup.tsx
@@ -11,11 +11,7 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li
-            className="list-group-item"
-            key={item}
-            onClick={(event) => console.log(event)}
-          >
+          <li className="list-group-item" key={item} onClick={handleClick}>
             {item}
           </li>
         ))}
