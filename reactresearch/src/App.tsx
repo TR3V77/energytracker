540e90e5b34467eda07686731986ce65ae3d6036
Bruce <czp18@txstate.edu>
2026-02-27 21:05:47 -0600

    Passing data via Props example

--- reactresearch/src/App.tsx
+++ reactresearch/src/App.tsx
@@ -1,10 +1,10 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
   return (
     <div>
-      <ListGroup />
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
