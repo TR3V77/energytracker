82b019330159d26661bccd97678b9717b61647f8
Bruce <czp18@txstate.edu>
2026-03-01 14:41:46 -0600

    Added new imports for App.tsx

--- reactresearch/src/App.tsx
+++ reactresearch/src/App.tsx
@@ -1,4 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import { useState } from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import EfficiencyRankings from "./components/EfficiencyRankings";
+import DataUpload from "./components/DataUpload";
+import TrendAnalysis from "./components/TrendAnalysis";
+import Recommendations from "./components/Recommendations";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   let items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
